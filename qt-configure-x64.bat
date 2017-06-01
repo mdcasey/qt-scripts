a0f87cd26bf1bf04c3f80497167af23a14631982
Matthew Casey <mdcasey@chabloom.com>
2017-06-01 16:52:23 -0400

    Update scripts for 5.9.0

--- qt-configure-x64.bat
+++ qt-configure-x64.bat
@@ -1,5 +1,5 @@
 @echo off
-set PATH=D:\build\qt-everywhere-opensource-src-5.8.0\gnuwin32\bin;D:\Programs\Python2;D:\build\qt-dep-x64;%PATH%
+set PATH=D:\build\qt-everywhere-opensource-src-5.9.0\gnuwin32\bin;D:\Programs\Python2;D:\build\qt-dep-x64;%PATH%
 set LIB=D:\build\qt-dep-x64;%LIB%
 set INCLUDE=D:\build\qt-dep-x64;%INCLUDE%
-..\qt-everywhere-opensource-src-5.8.0\configure -prefix D:\Programs\Qt\5.8.0\x64 -opensource -confirm-license -nomake examples -nomake tests -opengl dynamic -mediaplayer-backend wmf -proprietary-codecs -ltcg -openssl-linked OPENSSL_LIBS="-lgdi32 -luser32" OPENSSL_LIBS_DEBUG="-lssleay32d -llibeay32d" OPENSSL_LIBS_RELEASE="-lssleay32 -llibeay32" -mp
+..\qt-everywhere-opensource-src-5.9.0\configure -prefix D:\Programs\Qt\5.9.0\x64 -opensource -confirm-license -nomake examples -nomake tests -opengl dynamic -mediaplayer-backend wmf -proprietary-codecs -ltcg -openssl-linked OPENSSL_LIBS="-lgdi32 -luser32" OPENSSL_LIBS_DEBUG="-lssleay32d -llibeay32d" OPENSSL_LIBS_RELEASE="-lssleay32 -llibeay32" -mp
--- qt-configure-x86-uwp.bat
+++ qt-configure-x86-uwp.bat
@@ -1,3 +1,5 @@
 @echo off
-set PATH=D:\build\qt-everywhere-opensource-src-5.8.0\gnuwin32\bin;D:\Programs\Python2;%PATH%
-..\qt-everywhere-opensource-src-5.8.0\configure -prefix D:\Programs\Qt\5.8.0\x86-uwp -opensource -confirm-license -nomake examples -nomake tests -xplatform winrt-x86-msvc2015 -mediaplayer-backend wmf -proprietary-codecs -ltcg -directwrite -mp
+set PATH=D:\build\qt-everywhere-opensource-src-5.9.0\gnuwin32\bin;D:\Programs\Python2;D:\build\qt-dep-x86-uwp;%PATH%
+set LIB=D:\build\qt-dep-x86-uwp;%LIB%
+set INCLUDE=D:\build\qt-dep-x86-uwp;%INCLUDE%
+..\qt-everywhere-opensource-src-5.9.0\configure -prefix D:\Programs\Qt\5.9.0\x86-uwp -opensource -confirm-license -nomake examples -nomake tests -xplatform winrt-x86-msvc2017 -mediaplayer-backend wmf -proprietary-codecs -ltcg -directwrite -mp
